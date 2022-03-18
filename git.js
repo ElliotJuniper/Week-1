71858ed16a7b2c704482d27946e75a6e0ae9fbd6
Elliot Juniper <elliotjuniper@gmail.com>
2022-03-18 12:03:38 +0000

    Trial and Error

--- git.js
+++ git.js
@@ -3,3 +3,21 @@
 
 // Step 2
 // git config --global user.email elliotjuniper@gmail.com
+
+// Step 3
+// Use the commands:
+// git init
+// git status
+// git add app.js
+// git status
+// git commit -m “enter a comment here”
+// git status 
+
+// Step 4
+// Create remote repo on GitHub
+// git remote add origin https://github.com/ElliotJuniper/Example.git
+// git branch -M main
+// git push -u origin main
+// Copy token
+// paste in signing in bar
+// git push
